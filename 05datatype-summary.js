9178b6aa6618372d1d9175e98cfd1cc469460d18
sameerkpy <sameer.dev0740@gmail.com>
2026-06-17 23:21:43 +0530

    updated

--- 05datatype-summary.js
+++ 05datatype-summary.js
@@ -2,39 +2,39 @@
 
 // 7 type :String Number Boolean null undifined Symbol BignIt
 
-const score =100
-const scoreValue = 100.3
+// const score =100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp =null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp =null
+// let userEmail;
 
-const id =  Symbol ('123')
-const anotherId = Symbol ( '123')
+// const id =  Symbol ('123')
+// const anotherId = Symbol ( '123')
 
-console.log( id === anotherId)
+// console.log( id === anotherId)
 
-const bigNumber =4554655674546n
+// const bigNumber =4554655674546n
 
 // refrence (non primitive)
 
 // Array , Objects
  
-const heroes = ["shaktiman ","naagraj","doga" ]
-let myObj = {
-     name:"hitesh",
-     age:22,
-}
+// const heroes = ["shaktiman ","naagraj","doga" ]
+// let myObj = {
+//      name:"hitesh",
+//      age:22,
+// }
 
-const myFunction = function(){
-    console.log("hello wolrd");
-}
-console.log(typeof bigNumber);
+// const myFunction = function(){
+//     console.log("hello wolrd");
+// }
+// console.log(typeof bigNumber);
 
-console.log(typeof Function);
-console.log (typeof null)
-console.log(typeof Symbol)
-console.log(typeof false)
+// console.log(typeof Function);
+// console.log (typeof null)
+// console.log(typeof Symbol)
+// console.log(typeof false)
 
 
 
