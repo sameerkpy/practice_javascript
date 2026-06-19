a2c99fb9a08d7ebb42eea7cba48fb6c13494a39c
sameerkpy <sameer.dev0740@gmail.com>
2026-06-19 10:55:52 +0530

    updated

---
+++ problem.js
@@ -0,0 +1,63 @@
+// problem 1 
+let name = "sameer";
+let age = 21;
+let city = "jaipur";
+
+console.log(`my name is ${name}.`);
+console.log(`i am ${age} years old.`);
+console.log(`i live in ${city}.`);
+//   problem 2  
+let celsius = 25;
+let fahrenhiet = (celsius *9/5) +32;
+
+console.log(fahrenhiet+"F");
+
+// problem 3 
+
+let score = "100";
+let convertedscore = Number(score)
+
+ console.log(convertedscore + 50)
+
+//  problem 4
+let a = 10;
+let b = "10";
+
+console.log (a == b)
+console.log ( a === b)
+console.log( a > b)
+console.log (a<b)
+
+// problem 5 
+ let number = 17 ; 
+ if (number % 2===0){
+    console.log(number +"is even")
+ }
+ else{
+    console.log(number + "is odd"
+    )
+ }
+
+//  problem 6
+ let username = "sameerforge";
+ console.log(username.length)
+
+ //probme 7 
+ let word = "javaScript"
+ console.log(word[0])
+ console.log(word[word.length -1
+ ])
+// problem 8 
+let firstname = "sameer"
+let lastname = "khan"
+console.log(firstname +" "+lastname)
+
+// problem 9
+
+let username = "  SAMEERFORGE  ";
+
+let formattedUsername = username
+    .trim()
+    .toLowerCase();
+
+console.log(formattedUsername);
