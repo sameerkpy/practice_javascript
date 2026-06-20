39cdb2c5961eeab559c4e7c2a37dd65d8c83b4bb
sameerkpy <sameer.dev0740@gmail.com>
2026-06-20 12:14:49 +0530

    i have updated this code

--- problem.js
+++ problem.js
@@ -1,63 +1,84 @@
 // problem 1 
-let name = "sameer";
-let age = 21;
-let city = "jaipur";
+// let name = "sameer";
+// let age = 21;
+// let city = "jaipur";
 
-console.log(`my name is ${name}.`);
-console.log(`i am ${age} years old.`);
-console.log(`i live in ${city}.`);
+// console.log(`my name is ${name}.`);
+// console.log(`i am ${age} years old.`);
+// console.log(`i live in ${city}.`);
 //   problem 2  
-let celsius = 25;
-let fahrenhiet = (celsius *9/5) +32;
+// let celsius = 25;
+// let fahrenhiet = (celsius *9/5) +32;
 
-console.log(fahrenhiet+"F");
+// console.log(fahrenhiet+"F");
 
 // problem 3 
 
-let score = "100";
-let convertedscore = Number(score)
+// let score = "100";
+// let convertedscore = Number(score)
 
- console.log(convertedscore + 50)
+//  console.log(convertedscore + 50)
 
 //  problem 4
-let a = 10;
-let b = "10";
+// let a = 10;
+// let b = "10";
 
-console.log (a == b)
-console.log ( a === b)
-console.log( a > b)
-console.log (a<b)
+// console.log (a == b)
+// console.log ( a === b)
+// console.log( a > b)
+// console.log (a<b)
 
 // problem 5 
- let number = 17 ; 
- if (number % 2===0){
-    console.log(number +"is even")
- }
- else{
-    console.log(number + "is odd"
-    )
- }
+//  let number = 17 ; 
+//  if (number % 2===0){
+//     console.log(number +"is even")
+//  }
+//  else{
+//     console.log(number + "is odd"
+//     )
+//  }
 
 //  problem 6
- let username = "sameerforge";
- console.log(username.length)
+//  let username = "sameerforge";
+//  console.log(username.length)
 
- //probme 7 
- let word = "javaScript"
- console.log(word[0])
- console.log(word[word.length -1
- ])
+//  //probme 7 
+//  let word = "javaScript"
+//  console.log(word[0])
+//  console.log(word[word.length -1
+//  ])
 // problem 8 
-let firstname = "sameer"
-let lastname = "khan"
-console.log(firstname +" "+lastname)
+// let firstname = "sameer"
+// let lastname = "khan"
+// console.log(firstname +" "+lastname)
 
 // problem 9
 
-let username = "  SAMEERFORGE  ";
+// let username = "  SAMEERFORGE  ";
 
-let formattedUsername = username
-    .trim()
-    .toLowerCase();
+// let formattedUsername = username
+//     .trim()
+//     .toLowerCase();
 
-console.log(formattedUsername);
+// console.log(formattedUsername);
+
+// problem 10
+
+// let myDate = new Date()
+// console.log(myDate.toLocaleDateString);
+// console.log(myDate.toTimeString());
+// console.log(myDate.getHours());
+// console.log(myDate.getMinutes());
+// console.log(myDate.setSeconds())
+
+// problem 11
+// let birthYear = 2003;
+// let myCreatedDate = new Date()
+// console.log(myCreatedDate.toLocaleDateString(birthYear));
+
+// problem 12
+let today = new Date();
+let nextYear = new Date(today.getFullYear() + 1, 0, 1);
+
+console.log((nextYear - today) / ((1000 * 60 * 60 * 24)))
+
